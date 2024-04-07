16de1e6d93f8f8bd56decbb0cc008f407a15aae2
mariyaalv <pavlovskayama.maria@yandex.ru>
2024-04-07 15:42:37 +0300

    YP-36 Разработка компонента инпута

---
+++ client/src/components/base/Input/Input.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input v-bind:class="{ 'error' : error }" v-bind:value="value" @input="onInputHandler" type="text"/>
+    <p v-if="error">{{ error }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '',
+      error: null,
+    }
+  },
+  name: "Input",
+  props: {
+    label: String,
+    placeholder: String,
+    validationCallback: Function,
+  },
+  methods: {
+    onInputHandler(evt) {
+      const value = evt.target.value;
+      this.value = value;
+      this.error = this.validationCallback(value);
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./Input.scss";
+</style>
