1ddf250842245ec97913e77a34a6b3ecb8b0d22c
Ilya Pozvolenko <BinaryCompound2@gmail.com>
2024-05-12 21:44:41 +0300

    yp-36_input_component доработака кода,добавлен API composition

--- client/src/components/base/Input/Input.vue
+++ client/src/components/base/Input/Input.vue
@@ -1,35 +1,36 @@
 <template>
-  <div>
+  <div :class = "{'input_container' : input_container}">
     <label v-if="label">{{ label }}</label>
-    <input v-bind:class="{ 'error' : error }" v-bind:value="value" @input="onInputHandler" type="text"/>
+    <input :class="{ 'error' : error }" :value="value" @input="onInputHandler" type="text"/>
     <p v-if="error">{{ error }}</p>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  data() {
-    return {
-      value: '',
-      error: null,
-    }
-  },
   name: "Input",
   props: {
     label: String,
     placeholder: String,
     validationCallback: Function,
   },
-  methods: {
-    onInputHandler(evt) {
-      const value = evt.target.value;
-      this.value = value;
-      this.error = this.validationCallback(value);
-    }
-  },
-}
-</script>
+  setup(props) {
+    const value = ref('');
+    const error = ref(null);
 
-<style lang="scss" scoped>
-@import "./Input.scss";
-</style>
+    const onInputHandler = (evt) => {
+      const inputValue = evt.target.value;
+      value.value = inputValue;
+      error.value = props.validationCallback(inputValue);
+    };
+
+    return {
+      value,
+      error,
+      onInputHandler
+    };
+  },
+};
+</script>
