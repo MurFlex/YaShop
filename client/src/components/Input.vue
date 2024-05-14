67d64d037fcf78fd054a827b9ebc1b481215b93c
Ilya Pozvolenko <BinaryCompound2@gmail.com>
2024-05-14 19:19:11 +0300

    yp-36_input_component\Добавление API composition, внесение правок в scss

--- client/src/components/Input.vue
+++ client/src/components/Input.vue
@@ -1,78 +1,63 @@
-
 <template>
-<div :class="{ 'input-container': input_container }">
-    <label v-if="label" :class="{'input-container__label' : input-container__label}">{{ label }}</label>
-    <input :class="{ 'input-container__input': true, 'input-container__input--error': error }" :value="value" @input="onInputHandler" type="text"/>
-    <p :class="{ 'input-container__error-text': error }" v-if="error">{{ error }}</p>
+  <div class="input_container">
+    <label v-if="label" class="input-container__label">{{ label }}</label>
+    <input class="input-container__input" :class="{ 'input-container__error': error }" :value="value" @input="onInputHandler" type="text"/>
+    <p class="input-container__error-text" v-if="error">{{ error }}</p>
   </div>
 </template>
 
-<script>
+
+<script setup>
 import { ref } from 'vue';
 
-export default {
-  name: "Input",
-  props: {
-    label: String,
-    placeholder: String,
-    validationCallback: Function,
-  },
-  setup(props) {
-    const value = ref('');
-    const error = ref(null);
+const value = ref('');
+const error = ref(null);
 
-    const onInputHandler = (evt) => {
-      const inputValue = evt.target.value;
-      value.value = inputValue;
-      error.value = props.validationCallback(inputValue);
-    };
-
-    return {
-      value,
-      error,
-      onInputHandler
-    };
-  },
+const onInputHandler = (evt) => {
+  const inputValue = evt.target.value;
+  value.value = inputValue;
+  error.value = props.validationCallback(inputValue);
 };
 </script>
 
+
 <style lang="scss">
-input {
-  width: 100%;
-  border: 1px solid #d3dbe2;
-  border-radius: 3px;
-  padding: 5px;
-  height: 32px;
-  transition: 0.3s;
-  
-  &:focus {
-    border-color: #0388cc;
-    box-shadow: 0 0 3px #0388cc;
-    outline: none;
-  }
-}
-
-.error {
-  border-color: $error-color;
-
-  &:focus {
-    border-color: $error-color;
-    box-shadow: 0 0 3px $error-color;
-  }
-}
-
-.input-container__error-text {
-  color: $error-color;
-}
-
-.input-container__label {
-  color: #555;
-}
-
-.input_container {
+.input-container {
   @include main-size;
   display: flex;
   flex-direction: column;
   gap: 8px;
+
+  &__input {
+    width: 100%;
+    border: 1px solid #d3dbe2;
+    border-radius: 3px;
+    padding: 5px;
+    height: 32px;
+    transition: 0.3s;
+
+    &:focus {
+      border-color: #0388cc;
+      box-shadow: 0 0 3px #0388cc;
+      outline: none;
+    }
+
+    &.input-container__error {
+      border-color: $error-color;
+
+      &:focus {
+        border-color: $error-color;
+        box-shadow: 0 0 3px $error-color;
+      }
+    }
+  }
+
+  &__error-text {
+    color: $error-color;
+  }
+
+  &__label {
+    color: #555;
+  }
 }
 </style>
